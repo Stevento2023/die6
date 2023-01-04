2b9b2454d6b5035cbd7c0ba7f41d05c4d46a5da2
Steven To <steven.to@mrjaffesclass.com>
2023-01-04 11:16:06 -0800

    finished

--- die6-master/Yahtzee.java
+++ die6-master/Yahtzee.java
@@ -1,10 +1,3 @@
-
-/**
- * Write a description of class Yahtzee here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
 public class Yahtzee 
 {
     // instance variables - replace the example below with your own
@@ -50,9 +43,32 @@ public class Yahtzee
         
         for (int i=0; i<numberOfDice; i++) {
             int value = dice[i].getValue();
+            
+            if(value == 1) {
+                count[0]++;
+            } else if (value == 2) {
+                count[1]++;
+            } else if (value == 3) {
+                count[2]++;
+            } else if (value == 4) {
+                count[3]++;
+            } else if (value == 5) {
+                count[4]++;
+            } else if (value == 6) {
+                count[5]++;
+            }
+        }
+        
+        
+        //now youre gonna print result
+        for (int i=0; i<numberOfSides; i++) {
+            String stringValues = String.valueOf(i+1) + "-" + String.valueOf(count[i]);
+            if (i<numberOfSides - 1) {
+                stringValues= stringValues + "; ";
+            }
+            
+            str = str + stringValues;
         }
         return str;
     }
-    
-
 }
